6ad4f286a072cea6050929f0bb957ea8d3f5fc4f
Antti Jankeri <antti.jankeri@gmail.com>
2023-12-08 08:59:57 +0100

    feat: connect FE and BE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const connect = async () => {
+    const response = await fetch("http://localhost:3000/");
+    console.log(response);
+  };
+
+  useEffect(() => {
+    connect();
+  }, []);
 
   return (
     <>
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
